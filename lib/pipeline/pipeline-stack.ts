c83178a44945ff2cf4e9c842489b46a6dd6f4691
Thales Terra <thalesterra@cooxupe.com.br>
2023-03-24 15:34:31 -0300

    alteração da stack

--- lib/pipeline/pipeline-stack.ts
+++ lib/pipeline/pipeline-stack.ts
@@ -12,12 +12,13 @@ export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'CognitoPipeline'.concat(props.branch), {
-      pipelineName: 'CognitoPipeline'.concat(props.branch),
+    const pipeline = new CodePipeline(this, `pipeline-${props.branch}`, {
+      pipelineName: `pipeline-${props.branch}`,
       dockerEnabledForSynth: true,
       crossAccountKeys: true,
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('nbthales/CodePipeline', props.branch),
+        installCommands: ['npm i -g npm@latest'],
         commands: ['npm ci', 'npm run build', 'npx cdk synth'],
       }),
     });
